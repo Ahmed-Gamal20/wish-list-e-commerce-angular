e70a622bca7094c54a76cebf8a500d68c49d5b57
Ahmed-Gamal20 <93832798+Ahmed-Gamal20@users.noreply.github.com>
2024-08-09 03:30:57 +0300

    update local storage and fav count

--- src/app/screen/navbar/navbar.component.ts
+++ src/app/screen/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { FavoritesService } from '../../service/favorites.service';
 // import { Router } from 'express';
 
 @Component({
@@ -9,6 +10,16 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+
+  favoriteCount: number = 0;
+
+  constructor(private favoritesService: FavoritesService) {}
+
+  ngOnInit() {
+    this.favoritesService.getFavoriteCount().subscribe(count => {
+      this.favoriteCount = count;
+    });
+  }
 
 }
--- src/app/service/favorites.service.ts
+++ src/app/service/favorites.service.ts
@@ -1,20 +1,65 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FavoritesService {
 
-  constructor() { }
+ 
 
   private favorites: Set<number> = new Set();
+  private favoriteCount = new BehaviorSubject<number>(0);
+
+  // --------------------------------------------------------------------
+
+
+
+  // now
+  private storageKey = 'favoriteProducts';
+
+  // now
+  constructor() {
+    if (this.isBrowser()) {
+      this.loadFavoritesFromStorage();
+    }
+  }
+
+  private isBrowser(): boolean {
+    return typeof window !== 'undefined' && typeof localStorage !== 'undefined';
+  }
+
+  private loadFavoritesFromStorage() {
+    const storedFavorites = localStorage.getItem(this.storageKey);
+    if (storedFavorites) {
+      this.favorites = new Set(JSON.parse(storedFavorites));
+      this.updateFavoriteCount();
+    }
+  }
+
+
+  private saveFavoritesToStorage() {
+    localStorage.setItem(this.storageKey, JSON.stringify(Array.from(this.favorites)));
+  }
+
+
+
+
+
+  // ----------------------------------------------------------------------------------
 
   addFavorite(productId: number) {
     this.favorites.add(productId);
+    this.updateFavoriteCount();
+    // now
+    this.saveFavoritesToStorage();
   }
 
   removeFavorite(productId: number) {
     this.favorites.delete(productId);
+    this.updateFavoriteCount();
+    // now
+    this.saveFavoritesToStorage();
   }
 
   getFavorites(): number[] {
@@ -23,5 +68,11 @@ export class FavoritesService {
 
   isFavorite(productId: number): boolean {
     return this.favorites.has(productId);
+  }
+  getFavoriteCount() {
+    return this.favoriteCount.asObservable();
+  }
+  private updateFavoriteCount() {
+    this.favoriteCount.next(this.favorites.size);
   }
 }
